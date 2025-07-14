618a307198b12c31e9549b516378b15ad2a183be
Cody P Schafer <dev@codyps.com>
2025-07-14 18:08:25 -0400

    fix: use nixos rustPlatform, remove deprecated darwin.IOKit, allow overriding builtin workarounds

--- lib/deps_nix.ex
+++ lib/deps_nix.ex
@@ -196,6 +196,7 @@ defmodule DepsNix do
         pkgs,
         lib,
         beamPackages,
+        workaroundOverrides ? { },
         overrides ? (x: y: { }),
       }:
 
@@ -215,23 +216,9 @@ defmodule DepsNix do
             }:
             old:
             let
-              extendedPkgs = pkgs.extend fenixOverlay;
-              fenixOverlay = import "${
-                fetchTarball {
-                  url = "https://github.com/nix-community/fenix/archive/056c9393c821a4df356df6ce7f14c722dc8717ec.tar.gz";
-                  sha256 = "sha256:1cdfh6nj81gjmn689snigidyq7w98gd8hkl5rvhly6xj7vyppmnd";
-                }
-              }/overlay.nix";
               nativeDir = "${old.src}/native/${with builtins; head (attrNames (readDir "${old.src}/native"))}";
-              fenix =
-                if toolchain == null then
-                  extendedPkgs.fenix.stable
-                else
-                  extendedPkgs.fenix.fromToolchainName toolchain;
               native =
-                (extendedPkgs.makeRustPlatform {
-                  inherit (fenix) cargo rustc;
-                }).buildRustPackage
+                pkgs.rustPlatform.buildRustPackage
                   {
                     pname = "${old.packageName}-native";
                     version = old.version;
@@ -240,14 +227,14 @@ defmodule DepsNix do
                       lockFile = "${nativeDir}/Cargo.lock";
                     };
                     nativeBuildInputs = [
-                      extendedPkgs.cmake
-                    ] ++ extendedPkgs.lib.lists.optional extendedPkgs.stdenv.isDarwin extendedPkgs.darwin.IOKit;
+                      pkgs.cmake
+                    ];
                     doCheck = false;
                   };
 
             in
             {
-              nativeBuildInputs = [ extendedPkgs.cargo ];
+              nativeBuildInputs = [ pkgs.cargo ];
 
               env.RUSTLER_PRECOMPILED_FORCE_BUILD_ALL = "true";
               env.RUSTLER_PRECOMPILED_GLOBAL_CACHE_PATH = "unused-but-required";
@@ -285,7 +272,7 @@ defmodule DepsNix do
                 ${old.buildPhase}
               '';
             };
-        };
+        } // workaroundOverrides;
 
         defaultOverrides = (
           final: prev:
